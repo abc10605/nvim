6d37a89141e6c105722467fc3f2e922b2f0f47c1
abc10605 <chenghsuanho@gmail.com>
2022-03-19 21:21:55 +0800

    my settings

---
+++ lua/user/lsp/settings/gopls.lua
@@ -0,0 +1,12 @@
+vim.cmd([[
+    augroup gofmt
+        autocmd!
+        autocmd BufWritePost *.go silent! !go fmt %
+        autocmd BufWritePost *.go edit
+        autocmd BufWritePost *.go redraw!
+    augroup END
+]])
+
+return {
+    cmd = { "gopls" }
+}
