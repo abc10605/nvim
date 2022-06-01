7fb3d27071c9c223470fbd8cb178ef4546f7a8aa
abc10605 <chenghsuanho@gmail.com>
2022-06-01 09:38:46 +0800

    refactor(dadbod): move autocmd to cmp.lua

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -128,3 +128,11 @@ cmp.setup {
     -- native_menu = false,
   },
 }
+
+-- dadbod-completion
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "sql", "mysql", "plsql" },
+  callback = function ()
+    require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
+  end
+})
